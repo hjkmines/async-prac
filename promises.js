338cd892e3eaea40a94bd8bfb0d1e05c8ca62551
Tony Kim <hjkmines@gmail.com>
2020-08-05 22:59:05 -0600

    sub classes

---
+++ promises.js
@@ -0,0 +1,26 @@
+class Book {
+  constructor(title, author, year) {
+    this.title = title, 
+    this.author = author, 
+    this.year = year 
+  }
+
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year} year`
+  }
+
+  revise(newYear) {
+    this.year = newYear; 
+    this.revised = true; 
+  }
+
+  static topBookStore() {
+    return 'Barnes and Noble'
+  }
+}
+
+//instantiate an object 
+const book1 = new Book('book 1', 'John Doe', '2015')
+
+book1.revise("1995")
+console.log(Book.topBookStore())
